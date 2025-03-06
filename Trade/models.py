c1ae20e1634f225d2a89979c14b79133e041f4d8
Amrita-Dey3792 <amrita15-3792@diu.edu.bd>
2025-03-06 07:57:39 +0600

    Create TradeData and Serializer

---
+++ Trade/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class TradeData(models.Model):
+    date = models.DateField()
+    trade_code = models.CharField(max_length=20)
+    high = models.DecimalField(max_digits=10, decimal_places=2)
+    low = models.DecimalField(max_digits=10, decimal_places=2)
+    open = models.DecimalField(max_digits=10, decimal_places=2)
+    close = models.DecimalField(max_digits=10, decimal_places=2)
+    volume = models.BigIntegerField()
+
+    def __str__(self):
+        return f"{self.trade_code} - {self.date}"
